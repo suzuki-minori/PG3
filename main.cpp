1f3b76c1f523ba24154f1822469b34363065ca67
ni4nn <146914397+ni4nn@users.noreply.github.com>
2024-10-16 15:12:41 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 #include<iostream>
-#include<Windows.h>
+#include<stdio.h>
+
+int wages(int n) {
+	if (n <= 1) {
+		return (100);
+	}
+		return(wages(n-1)* 2 - 50);
+}
+
 
 int main() {
-	char str[] = "おなかすいた";
-	printf("%s", str);
-	return 0;
+	int result;
+	int Result;
+	for (int n = 1; n <= 15; n++) {
+		result = wages(n);
+		printf("再帰的な%d時間の給料=%d\n", n, result);
+	}
+	for (int n = 1; n <= 15; n++) {
+		Result = 1072 * n;
+		printf("一般的な%d時間の給料=%d\n", n, Result);
+	}
+
+	return (0);
 }
