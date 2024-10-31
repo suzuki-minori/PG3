ecf224cf9edae5fa8b7129d2afe668ff354672f7
ni4nn <146914397+ni4nn@users.noreply.github.com>
2024-10-31 14:50:58 +0900

    kari

--- main.cpp
+++ main.cpp
@@ -3,41 +3,51 @@
 #include<Windows.h>
 #include<time.h>
 #include<stdlib.h>
+#include<functional>
 
 
-typedef void(*DFunc)(int*);
 
-void  DispResult(int* s) {
+void  DispResult(int* s,int*key) {
 	
+	int dice = rand() % 6;
+
+	if (dice == *key) {
+		if (dice == 0)
+			printf("正解:");
+			printf("出目は%d\n", dice);
+	}else {
+		if (dice == 1)
+			printf("不正解:");
+			printf("出目は%d\n", dice);
+		}
 }
 
-void setTimeout(DFunc d, int second) {
-	Sleep(second * 1000);
-	d(&second);
+void setTimeout(std::function<void(int*,int*)>p, int second,int key) {
+	
+	for (int i = 0; i < second; i++) {
+		Sleep(1000);
+		printf("%d...\n", second - i);
+	}
+	p(&second, &key);
 }
 
 int main() {
-	int dice;
-	int n;
+	
+	int key;
 
 	printf("サイコロを振るので、出目が半(奇数)か丁(偶数)かを当ててください\n");
 
-	srand((unsigned int)time(NULL));
-	dice = rand() % 6 + 1;
-
+	srand(static_cast<unsigned int>(time(NULL)));
+	
 	printf("半(奇数)か丁(偶数)か予想してください\n");
-	printf("半(奇数)だと思うなら1、丁(偶数)だと思うなら2を入力してください:");
-	scanf_s("%d", &n);
+	printf("半(奇数)だと思うなら0、丁(偶数)だと思うなら1を入力してください:");
+	scanf_s("%d", &key);
 
-	setTimeout(DispResult, 3);
+	std::function<void(int*, int*)>p = [](int* s, int* key) {DispResult(s, key); };
 
-	if ((dice == 1 && n == 1) || (dice == 2 && n == 2) || (dice == 3 && n == 1) || (dice == 4 && n == 2) || (dice == 5 && n == 1) || (dice == 6 && n == 2)) {
-		printf("正解:");
-		printf("出目は%d\n", dice);
-	}
-	else {
-		printf("不正解:");
-		printf("出目は%d\n", dice);
-	}
+
+
+	setTimeout(p,3,key);
+
 	return 0;
 }
