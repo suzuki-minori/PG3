e86c07ff6edbb3f02c5dfce3b9a7a071d4131dd7
ni4nn <146914397+ni4nn@users.noreply.github.com>
2025-02-12 16:18:22 +0900

    ok

--- main.cpp
+++ main.cpp
@@ -1,36 +1,47 @@
-#include <cstdio>
+#include <stdio.h>
+#include <list>
+#include <iostream>
+
 using namespace std;
 
-class Animal {
-public:
-    virtual void sound() const = 0;
-};
+int main(void) {
+
+	list <const char*> eki_list{
+		"Tokyo", "Kanda", "Akihabara", "Okachimachi", "Ueno", "Uguisudani",
+		"Nippori", "Tabata", "Komagome", "Sugamo", "Otsuka", "Ikebukuro", "Mejiro",
+		"Takadanobaba", "Sin-Okubo", "Shinjuku", "Yoyogi", "Harajuku", "Shibuya",
+		"Ebisu", "Meguro", "Gotanda", "Osaki", "Sinagawa", "Tamachi", "Hamamatsucho",
+		"Shimbashi", "Yurakucho"
+	};
 
 
-class Dog : public Animal {
-public:
-    void sound() const override {
-        printf("イヌ鳴き声：ワン\n");
-    }
-};
+	printf("1970年\n");
+	for (list<const char*>::iterator it_f = eki_list.begin(); it_f != eki_list.end(); it_f++) {
+		std::cout << *it_f << endl;
+	}
 
+	printf("\n2019年\n");
+	list<const char*>::iterator itr;
+	for (list<const char*>::iterator it_f = eki_list.begin(); it_f != eki_list.end(); ++it_f) {
 
-class Cat : public Animal {
-public:
-    void sound() const override {
-        printf("ネコ鳴き声:ニャー\n");
-    }
-};
+		if (*it_f == "Tabata") {
+			it_f = eki_list.insert(it_f, "Nishi-Nippori");
+			std::cout << *it_f << endl;
+			++it_f;
+		}
+		std::cout << *it_f << endl;
+	}
 
-int main() {
-    Animal* animal1 = new Dog();
-    Animal* animal2 = new Cat();
+	printf("\n2022年\n");
+	for (list<const char*>::iterator it_f = eki_list.begin(); it_f != eki_list.end(); ++it_f) {
 
-    animal1->sound();
-    animal2->sound();
+		if (*it_f == "Tamachi") {
+			it_f = eki_list.insert(it_f, "Takanawa Gateway");
+			std::cout << *it_f << endl;
+			++it_f;
+		}
+		std::cout << *it_f << endl;
+	}
 
-    delete animal1;
-    delete animal2;
-
-    return 0;
+	return 0;
 }
