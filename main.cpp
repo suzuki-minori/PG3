43a454edef38b1f2c69c8ff4d3fecf9d209355f0
ni4nn <146914397+ni4nn@users.noreply.github.com>
2025-02-13 17:35:50 +0900

    ok

--- main.cpp
+++ main.cpp
@@ -1,22 +1,57 @@
-#include <iostream>
-#include <thread>
+﻿#include <iostream>
+#include <string>
+#include <chrono>
 
-using namespace std;
+//コピー時間計算出力
+void measureCopyTime(const std::string & str) {
 
-void PrintThread(uint32_t num) {
+    //開始
+    auto start = std::chrono::high_resolution_clock::now();
 
-	cout << "thread" << num << endl;
+    //内容コピー
+    std::string copy = str;
+
+    //終了
+    auto end = std::chrono::high_resolution_clock::now();
+
+    //時間計算
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+
+    //出力
+    std::cout << "コピー： " << duration << "μs" << std::endl;
+}
+
+//移動時間計算＋出力
+void measureMoveTime(std::string&& str) {
+
+    //開始
+    auto start = std::chrono::high_resolution_clock::now();
+
+    //内容移動
+    std::string moved = std::move(str);
+
+    //終了
+    auto end = std::chrono::high_resolution_clock::now();
+
+    //時間計算
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+
+    //出力
+    std::cout << "移動： " << duration << "μs" << std::endl;
 }
 
 int main() {
 
-	// マルチスレッドではある
-	thread t1(PrintThread, 1);
-	t1.join();
-	thread t2(PrintThread, 2);
-	t2.join();
-	thread t3(PrintThread, 3);
-	t3.join();
+    //文字列string with 1000,000 'a'
+    std::string a(1000000, 'a');
 
-	return 0;
+    std::cout << "1000000文字を移動とコピーで比較" << std::endl;
+
+    //コピー
+    measureCopyTime(a);
+
+    //移動
+    measureMoveTime(std::move(a));
+
+    return 0;
 }
